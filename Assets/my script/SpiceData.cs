976532051e02049a33a4ede2840491f78f3a827e
Ayuta064 <ayuta0604@gmail.com>
2025-12-04 16:57:01 +0900

    配置仮決定

--- Assets/my script/SpiceData.cs
+++ Assets/my script/SpiceData.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 [Serializable]
 public class SpiceData
 {
-    // マーカー/QRコードの内容 (例: "SALT", "SUGAR")
+    [Tooltip("マーカー/QRコードの内容 (例: \"SALT\", \"SUGAR\")")]
     public string QrCodeData; 
     
-    // 調味料の名前 (Inspectorで表示される)
+    [Tooltip("調味料の名前 (Inspectorで表示される)")]
     public string SeasoningName; 
     
-    // 追跡用のホログラムオブジェクト (ハイライト表示用)
+    [Tooltip("追跡用のホログラムオブジェクト (ハイライト表示用)")]
     public GameObject HighlightObject; 
 
     // アンカーが登録済みであるかを示すフラグ
